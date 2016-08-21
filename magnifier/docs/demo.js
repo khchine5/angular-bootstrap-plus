87d202ffe2d5c43995cae692751fd0617a88d9e6
Hamza Khchine <hamzakhchine@gmail.com>
2016-08-21 19:01:24 +0100

    Update magnifier for github page

---
+++ magnifier/docs/demo.js
@@ -0,0 +1,10 @@
+/**
+	* Created by khchine on 8/21/16.
+	*/
+
+console.log('BootstrapPlusDemo_magnifier');
+
+var app  = angular.module('BootstrapPlusDemo', ['bs-plus.scrollpanel', 'bs-plus.select', 'bs-plus.processbar', 'bs-plus.magnifier'])
+
+app.controller('ExampleCtrl', ['$scope', function ($scope) {
+}]);
